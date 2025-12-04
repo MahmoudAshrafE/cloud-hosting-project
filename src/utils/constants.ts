f938d0177691308d860fec9244e7f8c2c49bf3d8
MahmoudAshrafE <mamo056998@gmail.com>
2025-12-04 11:45:29 +0200

    Domain updated

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,2 @@
 export const ARTICLE_PER_PAGE = 6
-export const DOMAIN =
-  process.env.NEXT_PUBLIC_DOMAIN ?? "https://cloud-hosting-project-lyart.vercel.app";
+export const DOMAIN = 'https://cloud-hosting-project-lyart.vercel.app'
