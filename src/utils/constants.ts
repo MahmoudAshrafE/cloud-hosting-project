5a7383eed2b10adf40445feba3daa1026596f44f
MahmoudAshrafE <mamo056998@gmail.com>
2025-12-04 11:40:01 +0200

    Domain added

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,2 +1,3 @@
 export const ARTICLE_PER_PAGE = 6
-export const DOMAIN = 'http://localhost:3000'
+export const DOMAIN =
+  process.env.NEXT_PUBLIC_DOMAIN ?? "https://cloud-hosting-project-lyart.vercel.app";
